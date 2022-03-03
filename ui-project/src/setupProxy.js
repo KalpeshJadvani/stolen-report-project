74228b60061a79ae75cbac24c8e87b770299a254
mac-kalpesh <mac-kalpesh@kalpehsMac.local>
2022-03-03 22:53:44 +0000

    Updated  backend url

--- ui-project/src/setupProxy.js
+++ ui-project/src/setupProxy.js
@@ -3,12 +3,12 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 // to proxy all api call to server in dev
 // this will useful in dev mode
 // but on production we can proxy servers
-
+//http://localhost:8484 
 module.exports = (app) => {
   app.use(
     '/api',
     createProxyMiddleware({
-      target: 'http://localhost:8484',
+      target: 'https://stolenreportbackend.herokuapp.com/',
       changeOrigin: true,
       pathRewrite: {
         '^/api': '/',
